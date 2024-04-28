7ea71b32144a60ce99f9b8afb323865dd72a6227
EHackC <lolisfun0987@gmail.com>
2024-04-28 12:37:52 +0900

    inject.js 생성

---
+++ inject.js
@@ -0,0 +1,18 @@
+function injectScript(path) {
+    let element = document.createElement("script");
+    element.setAttribute("src", path);
+    element.setAttribute("tpye", "module");
+    document.getElementsByTagName("body")[0].appendChild(element);
+}
+
+function injectCSS(path) {
+    let element = document.createElement("link");
+    element.setAttribute("href", path);
+    element.setAttribute("rel", "stylesheet");
+    document.getElementsByTagName("body")[0].appendChild(element);
+}
+
+if (window.location.pathname == "/community/entrystory/list") {
+    injectScript(chrome.runtime.getURL("entrystory/entrystory.js"));
+    injectCSS(chrome.runtime.getURL("entrystory/entrystory.css"));
+}
