e6366af946bdf1d28014d6891a25a0ef11e54072
EHackC <lolisfun0987@gmail.com>
2024-04-28 16:38:40 +0900

    이미지 업로드 기능 추가

--- entrystory/entrystory.js
+++ entrystory/entrystory.js
@@ -1,3 +1,13 @@
+let textarea;
+
+async function uploadImage(files) {
+    const file = files[0];
+    let imageData = await uploadImageFile(file, "notcompress", false);
+    let filename = imageData.filename;
+    let imageUrl = "playentry.org//uploads/" + filename.substring(0, 2) + "/" + filename.substring(2,4) + "/" + filename + "." + imageData.imageType;
+    textarea.innerText += imageUrl;
+}
+
 window.onload = function() {
     // 이미지 업로드 버튼 생성
     let imageUploadButton = document.createElement("label");
@@ -12,10 +22,13 @@ window.onload = function() {
     imageUploadButtonInput.setAttribute("type", "file");
     imageUploadButtonInput.setAttribute("class", "blind");
     imageUploadButtonInput.setAttribute("accept", ".png, .jpg, .jpeg, .bmp, .svg");
+    imageUploadButtonInput.setAttribute("onchange", "uploadImage(this.files)")
 
     imageUploadButton.appendChild(imageUploadButtonSpan);
     imageUploadButton.appendChild(imageUploadButtonInput);
 
     let toolBar = document.querySelector(".e1h77j9v7");
     toolBar.prepend(imageUploadButton);
+
+    textarea = document.querySelector(".e1h77j9v1 > textarea");
 };
--- fetch.js
+++ fetch.js
@@ -1,1 +1,16 @@
+let a;
+async function uploadImageFile(file, type, generateThumb) {
+    let data = new FormData();
+    data.append("file", file);
+    data.append("type", type);
+    data.append("generateThumb", generateThumb);
 
+    let r = await fetch("/rest/picture", {
+        method: "POST",
+        headers: {},
+        body: data
+    });
+    let imageData = await r.json();
+
+    return imageData;
+}
